69277b27cc9389ddad4f133b337195ebb1ee5f1f
muhammedashir170 <muhammedashir170.com>
2026-02-10 11:46:05 +0530

    initail

---
+++ theme_fresh_marts/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Developed by Xapp
+
+
+"""
+FreshMart Theme Module
+=====================
+Custom theme for Odoo eCommerce platform tailored for online grocery stores and supermarkets.
+"""
---
+++ theme_fresh_marts/__manifest__.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Developed by XAPP.
+
+{
+    'name': 'FreshMart',
+    'category': 'Theme',
+    'version': '18.0.1.0.0',
+    'sequence': 1,
+    'author': 'XAPP',
+    'summary': 'FreshMart - Odoo eCommerce Theme for Online Grocery Stores and Supermarkets',
+    'description': """
+        FreshMart - Website Theme for Odoo 18
+
+      This Odoo grocery theme delivers a fast, clean, and user-friendly storefront tailored for supermarkets and convenience stores. 
+      It includes optimized product sections, category highlights, 
+      eye-catching banners, and a simple checkout flow designed to 
+      increase conversions and enhance customer engagement.
+
+        Support:
+        -
+        For any questions or support, please contact.
+    """,
+    'odoo_online': False,
+    'license': 'LGPL-3',
+    'support': 'teamxapp.dev@gmail.com',
+    'live_test_url': 'https://freshmart.xappco.site/',
+    'price': 10.00,
+    'currency': 'USD',
+    'depends': [
+        'website',
+        'web_editor',
+        'website_sale',
+        'sale_management',
+        'crm',
+    ],
+    'data': [
+        'views/templates.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            # Theme CSS Files
+            'theme_fresh_marts/static/src/css/header.css',
+            'theme_fresh_marts/static/src/css/banner.css',
+            'theme_fresh_marts/static/src/css/categories.css',
+            'theme_fresh_marts/static/src/css/product.css',
+            'theme_fresh_marts/static/src/css/shop.css',
+            'theme_fresh_marts/static/src/css/cart.css',
+            'theme_fresh_marts/static/src/css/myhome.css',
+            'theme_fresh_marts/static/src/css/checkout.css',
+            'theme_fresh_marts/static/src/css/footer.css',
+            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+            # Theme JavaScript Files
+            'theme_fresh_marts/static/src/js/header.js',
+            'theme_fresh_marts/static/src/js/swiper-banner.js',
+            'theme_fresh_marts/static/src/js/website_sale.js',
+            'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js'
+        ],
+    },
+
+    'images': [
+        'static/description/freshmart_cover.gif',
+        'static/description/freshmart_screenshot.gif',
+
+    ],
+
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
