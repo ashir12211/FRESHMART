6c57be56fca6f99d66174186c80e4008cca8113b
MUHAMMED ASHIR <muhammedashir57@gmail.com>
2026-02-17 10:48:54 +0530

    Update live test URL in manifest file

--- theme_fresh_marts/__manifest__.py
+++ theme_fresh_marts/__manifest__.py
@@ -23,7 +23,7 @@
     'odoo_online': False,
     'license': 'LGPL-3',
     'support': 'teamxapp.dev@gmail.com',
-    'live_test_url': 'https://freshmart1.xappco.site/',
+    'live_test_url': 'https://freshmarts.xappco.site/',
     'price': 109.00,
     'currency': 'USD',
     'depends': [
